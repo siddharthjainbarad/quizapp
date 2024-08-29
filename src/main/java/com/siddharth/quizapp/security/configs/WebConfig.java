4b3e5852923403728d2e9f459497a0d4b0639585
Siddharth Jain <siddharthisnext@gmail.com>
2024-08-29 12:32:22 +0530

    14-add:  chore: Update application.properties and add LoggingInterceptor and WebConfig

---
+++ src/main/java/com/siddharth/quizapp/security/configs/WebConfig.java
@@ -0,0 +1,20 @@
+package com.siddharth.quizapp.security.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.siddharth.quizapp.security.LoggingInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
